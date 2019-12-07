cd3c6143e406710a026abba1e7fa6741bd9280d4
accelsao <bebe8277@gmail.com>
2019-12-07 21:56:01 +0800

    add cropImage window

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # PhotoMontage
 ## TODO
-- [ ] 一開始 Arrow 設定不可見 當滑鼠 (scene) 移動到 arrow 附近
-arrow 可視 同時當mouse pressed 的時候 開始記錄位置變化 並且 update scene (先確認可不可以 update)
-一切都在 rectangle 上操作座標位置 arrow 只負責可視和不可視(set event mode) 並且固定位置
+- [ ] 先寫兩個 arrow
 - [ ] crop images in new window before loading 
 - [ ] another view before first background , maybe set the welcome images and fix size at first
 - [ ] images lists selection connect to resize images
--- unit_test_5.py
+++ unit_test_5.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtCore import QRectF, Qt, QPoint, QPointF, QRect
 from PyQt5.QtGui import QPen, QBrush, QPixmap, QPainter, QImage
 from PyQt5.QtWidgets import QApplication, QWidget, QGraphicsView, QGraphicsScene, QFrame, QGraphicsPixmapItem, \
-    QGraphicsItem, QStyleOptionGraphicsItem, QGraphicsSceneMouseEvent
+    QGraphicsItem, QStyleOptionGraphicsItem, QGraphicsSceneMouseEvent, QGraphicsRectItem
 
 
 class CropImage(QGraphicsPixmapItem):
@@ -13,11 +13,13 @@ class CropImage(QGraphicsPixmapItem):
         self.img = QPixmap('images/cat.jpg').scaledToHeight(720)
         h, w = self.img.height(), self.img.width()
         self.board = 100
-        bg_img = QImage(w + self.board * 2, h + self.board * 2, QImage.Format_RGB32)
-        bg_img.fill(Qt.white)
-        self.background = QPixmap.fromImage(bg_img)
+        # bg_img = QImage(w + self.board * 2, h + self.board * 2, QImage.Format_RGB32)
+        # bg_img.fill(Qt.white)
+        # self.background = QPixmap.fromImage(bg_img)
 
-        self.new_img_pos_x, self.new_img_pos_y = self.img.width(), self.img.height()
+        # self.new_img_pos_x, self.new_img_pos_y = self.img.width(), self.img.height()
+
+        self.currentTopLeft = QPointF(self.board, self.board)
 
         self.mainImg = QPixmap()
 
@@ -26,16 +28,7 @@ class CropImage(QGraphicsPixmapItem):
         self.paint(painter, styleoptiongrahpicsitem)
 
     def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
-        # print(self.new_img_pos_x)
-        # print(self.img.width())
-        painter.drawPixmap(
-            # QPointF(self.new_img_pos_x - self.img.width() / 2, self.new_img_pos_y - self.img.height() / 2),
-            # QRectF(0, 0, self.img.width(), self.img.height()),
-            # QPointF(-self.img.width() / 2, -self.img.height() / 2),
-            QPointF(0, 0),
-            self.background
-        )
-        print(self.mainImg.size())
+        print('RePaint')
         painter.drawPixmap(
             # QPointF(self.new_img_pos_x - self.img.width() / 2, self.new_img_pos_y - self.img.height() / 2),
             # QRectF(0, 0, self.img.width(), self.img.height()),
@@ -43,7 +36,7 @@ class CropImage(QGraphicsPixmapItem):
             QPointF(self.board, self.board),
             self.img
         )
-        print(self.mainImg.size())
+        # print(self.mainImg.size())
         # painter.drawPixmap(
         #     QRect(0, 0, self.img.width() + self.board * 2, self.img.height() + self.board * 2), QPixmap()
         # )
@@ -58,53 +51,135 @@ class CropImage(QGraphicsPixmapItem):
         self.setPixmap(self.img)
         # print(123)
 
+    def mouseMoveEvent(self, e):
+        print(123)
+
+
 class ArrowIcon(QGraphicsPixmapItem):
 
     def __init__(self, icon_path='images/righttop-leftbot.png'):
         super(ArrowIcon, self).__init__()
         arrow_pixmap = QPixmap(icon_path).scaledToWidth(50)
         self.setPixmap(arrow_pixmap)
-        # self.arrow = arrow
-        # self.setSelected(True)
-        # self.setAcceptHoverEvents(True)
+        self.startPos = self.pos()
+        self.updatePos = False
 
     def mousePressEvent(self, e):
-        print('selected')
+        self.updatePos = True
+        # self.startPos = e.pos()
 
+    def mouseMoveEvent(self, e):
+        # if self.updatePos
+        pass
+        # self.currentPos = e.pos()
+        # self.newImagePos = QPointF(self.currentPos.x() + self.imgPosX)
+
+    def mouseReleaseEvent(self, e):
+        self.updatePos = False
 
 
 class MyScene(QGraphicsScene):
-    def __init__(self, img_pixmap):
+    def __init__(self, imgPixmap):
         """
 
         :param img: (QGraphicsPixmapItem) main image
         """
         super(MyScene, self).__init__()
 
-        h, w = img_pixmap.height(), img_pixmap.width()
-        img = QGraphicsPixmapItem(img_pixmap)
+        self.imgW, self.imgH = imgPixmap.width(), imgPixmap.height()
+        self.img = QGraphicsPixmapItem(imgPixmap)
 
-        board = 100
+        # board = 100
 
         # # arrow
         # arrow_pixmap = QPixmap('images/righttop-leftbot.png').scaledToWidth(50)
         # arrow = QGraphicsPixmapItem(arrow_pixmap)
         # arrow.setPos(-25, -25)
-        arrow_icon = ArrowIcon()
+        self.arrow_icon_top_left = ArrowIcon()
+        self.arrow_icon_bottom_right = ArrowIcon()
+
+
+
         # arrow.mousePressEvent.connect(self.arrow_mouse_pressed)
 
         # rectangle
-        qPen = QPen(Qt.red)
-        qPen.setWidth(5)
+        self.rectPen = QPen(Qt.red)
+        self.rectPen.setWidth(5)
 
-        self.addItem(img)
-        self.addRect(QRectF(10, 10, w, h), qPen)
-        self.addItem(arrow_icon)
+        self.addItem(self.img)
+        # self.rectCropArea = QGraphicsRectItem(QRectF(0, 0, self.imgW, self.imgH))
+        self.rectTopLeft = QPointF(0, 0)
+        self.rectBottomRight = QPointF(self.imgW, self.imgH)
+        self.rectCropArea = QGraphicsRectItem(QRectF(self.rectTopLeft, self.rectBottomRight))
+        self.rectCropArea.setPen(self.rectPen)
+        self.addItem(self.rectCropArea)
+
+        self.arrow_icon_top_left.setPos(-25, -25)
+        self.arrow_icon_bottom_right.setPos(self.imgW - 25, self.imgH - 25)
+
+        self.addItem(self.arrow_icon_top_left)
+        self.addItem(self.arrow_icon_bottom_right)
+
+        self.updateMode = 0
+
+
+
+        # mouseEvent
+        # self.rectPos = QPointF()
+        # self.newRectPos = self.rectPos
+        self.startPos = QPointF()
+
+        # self.prevRectTopLeft = self.rectTopLeft
+        self.prevRectTopLeft = QPointF(self.rectTopLeft)
+        self.prevRectBottomRight = QPointF(self.rectBottomRight)
+
+        self.minSizeofRect = 64
 
     def mousePressEvent(self, e):
-        super(MyScene, self).mousePressEvent(e)
+        if self.arrow_icon_top_left.x() <= e.scenePos().x() <= self.arrow_icon_top_left.x() + self.arrow_icon_top_left.pixmap().width() and \
+                self.arrow_icon_top_left.y() <= e.scenePos().y() <= self.arrow_icon_top_left.y() + self.arrow_icon_top_left.pixmap().height():
+            self.updateMode = 1
+            self.startPos = e.scenePos()
+        elif self.arrow_icon_bottom_right.x() <= e.scenePos().x() <= self.arrow_icon_bottom_right.x() + self.arrow_icon_bottom_right.pixmap().width() and \
+                self.arrow_icon_bottom_right.y() <= e.scenePos().y() <= self.arrow_icon_bottom_right.y() + self.arrow_icon_bottom_right.pixmap().height():
+            self.updateMode = 2
+            self.startPos = e.scenePos()
+        else:
+
+            self.updateMode = 0
+
+    def mouseMoveEvent(self, e):
+        if self.updateMode == 1:
+            self.currentPos = e.scenePos()
+            self.rectTopLeft.setX(min(max(self.prevRectTopLeft.x() + self.currentPos.x() - self.startPos.x(), 0.0),
+                                         self.prevRectBottomRight.x() - self.minSizeofRect))
+
+            self.rectTopLeft.setY(min(max(self.prevRectTopLeft.y() + self.currentPos.y() - self.startPos.y(), 0.0),
+                                         self.prevRectBottomRight.y() - self.minSizeofRect))
+            self.update()
+        elif self.updateMode == 2:
+            self.currentPos = e.scenePos()
+            self.rectBottomRight.setX(max(min(self.prevRectBottomRight.x() + self.currentPos.x() - self.startPos.x(), self.imgW),
+                                      self.prevRectTopLeft.x() + self.minSizeofRect))
+
+            self.rectBottomRight.setY(max(min(self.prevRectBottomRight.y() + self.currentPos.y() - self.startPos.y(), self.imgH),
+                                      self.prevRectTopLeft.y() + self.minSizeofRect))
+            self.update()
 
 
+    def mouseReleaseEvent(self, e):
+
+        self.updateMode = False
+        self.prevRectTopLeft = QPointF(self.rectTopLeft)
+        self.prevRectBottomRight = QPointF(self.rectBottomRight)
+
+
+    def update(self):
+        self.rectCropArea.setRect(QRectF(self.rectTopLeft, self.rectBottomRight))
+        # print('Pass')
+        # self.arrow_icon.setPos(self.newRectTopLeft.x() - 25, self.newRectTopLeft.y() - 25)
+        self.arrow_icon_top_left.setPos(self.rectTopLeft.x() - 25, self.rectTopLeft.y() - 25)
+        self.arrow_icon_bottom_right.setPos(self.rectBottomRight.x() - 25, self.rectBottomRight.y() - 25)
 
 
 class MainWindow(QGraphicsView):
@@ -113,16 +188,13 @@ class MainWindow(QGraphicsView):
         # main image
         img_pixmap = QPixmap('images/cat.jpg').scaledToHeight(720)
         h, w = img_pixmap.height(), img_pixmap.width()
-        img = QGraphicsPixmapItem(img_pixmap)
+        # img = QGraphicsPixmapItem(img_pixmap)
         scene = MyScene(img_pixmap)
         self.setScene(scene)
         self.resize(w + 200, h + 200)
-        
-    def mousePressEvent(self, e):
-        super(MainWindow, self).mousePressEvent(e)
+
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
